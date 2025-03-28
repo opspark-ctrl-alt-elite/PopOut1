bcb334a42e1e7ecd0901b054cffbb5dc68e3d3c4
khamal22 <yyeah3198@gmail.com>
2025-03-28 13:30:22 -0400

    UPDATED: event model name

---
+++ server/models/EventModel.ts
@@ -0,0 +1,85 @@
+import sequelize from "./index";
+import { DataTypes, Model } from "sequelize";
+import Vendor from "./Vendor";
+
+export class Event extends Model {}
+
+Event.init(
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+    },
+    vendor_id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      references: {
+        model: "vendors",
+        key: "id",
+      },
+      onDelete: "CASCADE",
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    category: {
+      type: DataTypes.ENUM("Food & Drink", "Art", "Music", "Sports & Fitness", "Hobbies"),
+      allowNull: false,
+    },
+    startDate: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    endDate: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    venue_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    latitude: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+    longitude: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+    isFree: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+    isKidFriendly: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+    isSober: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+    image_url: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+  },
+  {
+    sequelize,
+    modelName: "Event",
+    tableName: "events",
+    timestamps: true,
+  }
+);
+
+// Event with Vendor
+Vendor.hasMany(Event, { foreignKey: "vendor_id", onDelete: "CASCADE" });
+Event.belongsTo(Vendor, { foreignKey: "vendor_id" });
+
+export default Event;
