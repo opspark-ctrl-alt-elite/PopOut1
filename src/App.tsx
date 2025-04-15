f489c6cafdb42426c4eaec1db801ee69c7e1c06a
khebbler <khebbler@gmail.com>
2025-04-15 16:15:40 -0500

    UPDATE: App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ import { registerServiceWorker } from "./firebase/sw-registration";
 import { requestNotificationPermission } from "./firebase/requestPermission";
 import PublicVendorProfile from "./components/PublicVendorProfile";
 import TopVendorSpotlight from "./components/TopVendorSpotlight";
+import NotificationListener from "./components/NotificationListener";
 import axios from "axios";
 
 type User = {
@@ -43,38 +44,32 @@ type Vendor = {
 type Captcha = {
   beatCaptcha: boolean;
   wantsToBeVendor: boolean;
-}
+};
 
 const App: React.FC = () => {
   const [user, setUser] = useState<User | null>(null);
   const [vendors, setVendors] = useState<Vendor[] | null>(null);
 
-  // stores whether or not user has beaten the captcha
   const [captcha, setCaptcha] = useState<Captcha>({
     beatCaptcha: false,
-    wantsToBeVendor: false
+    wantsToBeVendor: false,
   });
 
-  // a passed-down function to get and update the user state from children components
-  let getUser = async () => {
+  const getUser = async () => {
     try {
       const userObj = await axios.get(`/user/me`, {
         withCredentials: true,
       });
-      // set state's user value
       setUser(userObj.data);
     } catch (err) {
-      // set user to null when no vendor can be found
       setUser(null);
-      console.error("Error retrieving user record: ", err);
+      console.error("Error retrieving user record:", err);
     }
-  }
+  };
 
   useEffect(() => {
     registerServiceWorker();
-    requestNotificationPermission();
 
-    // get current user
     fetch("/auth/me", {
       credentials: "include",
     })
@@ -86,10 +81,9 @@ const App: React.FC = () => {
         if (data && data.id) setUser(data);
       })
       .catch((err) => {
-        // console.error(err);
+        console.error("Error fetching user:", err);
       });
 
-    // Get all vendor records
     fetch("/vendor/all")
       .then((res) => {
         if (!res.ok) throw new Error("err fetching vendors");
@@ -100,30 +94,36 @@ const App: React.FC = () => {
         if (data) setVendors(data);
       })
       .catch((err) => {
-        // console.error(err);
+        console.error("Error fetching vendors:", err);
       });
   }, []);
 
+  useEffect(() => {
+    if (user?.id) {
+      requestNotificationPermission(user.id);
+    }
+  }, [user]);
+
   return (
-    <Routes>
-      <Route path="/" element={<Home user={user} vendors={vendors} captcha={captcha} setCaptcha={setCaptcha}/>} />
-      <Route path="/map" element={<Map user={user} />} />
-      <Route path="/userprofile" element={<UserProfile user={user} />} />
-      <Route path="/edit-profile" element={<EditProfile user={user} />} />
-      <Route path="/vendorprofile" element={<VendorProfile user={user} getUser={getUser} />} />
-      <Route path="/vendor-signup" element={<VendorSignupForm user={user} getUser={getUser} captcha={captcha} setCaptcha={setCaptcha}/>} />
-      <Route path="/preferences" element={<Preferences setUser={setUser} />} />
-      <Route path="/create-event" element={<CreateEvent />} />
-      <Route path="/edit-event/:id" element={<EditEvent />} />
-      <Route path="/active-events" element={<ActiveEvents user={user} />} />
-      <Route path="/events" element={<EventsFeed />} />
-      <Route path="/game" element={<GameApp captcha={captcha} setCaptcha={setCaptcha} />} />
-      {/* Vendor Spotlight route */}
-      <Route path="/vendor-spotlight" element={<TopVendorSpotlight />} />
-      
-      {/* Public Vendor Profile Route (Uncommented) */}
-      <Route path="/vendor/:vendorId" element={<PublicVendorProfile user={user} />} />
-    </Routes>
+    <>
+      <NotificationListener />
+      <Routes>
+        <Route path="/" element={<Home user={user} vendors={vendors} captcha={captcha} setCaptcha={setCaptcha} />} />
+        <Route path="/map" element={<Map user={user} />} />
+        <Route path="/userprofile" element={<UserProfile user={user} />} />
+        <Route path="/edit-profile" element={<EditProfile user={user} />} />
+        <Route path="/vendorprofile" element={<VendorProfile user={user} getUser={getUser} />} />
+        <Route path="/vendor-signup" element={<VendorSignupForm user={user} getUser={getUser} captcha={captcha} setCaptcha={setCaptcha} />} />
+        <Route path="/preferences" element={<Preferences setUser={setUser} />} />
+        <Route path="/create-event" element={<CreateEvent />} />
+        <Route path="/edit-event/:id" element={<EditEvent />} />
+        <Route path="/active-events" element={<ActiveEvents user={user} />} />
+        <Route path="/events" element={<EventsFeed />} />
+        <Route path="/game" element={<GameApp captcha={captcha} setCaptcha={setCaptcha} />} />
+        <Route path="/vendor-spotlight" element={<TopVendorSpotlight />} />
+        <Route path="/vendor/:vendorId" element={<PublicVendorProfile user={user} />} />
+      </Routes>
+    </>
   );
 };
 
