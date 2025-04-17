4a6175433ccfbc110014158d8953b8c624775caf
khebbler <khebbler@gmail.com>
2025-04-16 19:40:55 -0500

    ADD: Theme folder, Fonts

---
+++ src/theme/index.ts
@@ -0,0 +1,15 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: `'Inter', sans-serif`,
+    h4: {
+      fontFamily: `'Bebas Neue', sans-serif`,
+    },
+    h6: {
+      fontFamily: `'Bebas Neue', sans-serif`,
+    },
+  },
+});
+
+export default theme;
